b8fd9c28f8028b04424e72fdcb20012af96b3ea0
yoonsangmin <dbstkdalsa@naver.com>
2022-02-22 00:34:23 +0900

    완전 탐색 한 문제 품
    숫자 조합 문제 두 번째 푸는데 좀 더 익숙해지도록 노력하기

---
+++ Coding-Test-Practice-C++/Programmers L2 - Finding Prime Number.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int solution(string numbers) {
+	int answer = 0;
+	set<int> number_set;
+
+	sort(numbers.begin(), numbers.end());
+
+	for (int r = 1; r <= numbers.size(); r++)
+	{
+		vector<bool> comb(numbers.size(), false);
+		for (int i = 0; i < r; i++)
+		{
+			comb[numbers.size() - 1 - i] = true;
+		}
+
+		do
+		{
+			string number_string = "";
+
+			for (int i = 0; i < numbers.size(); i++)
+			{
+				if (comb[i])	number_string += numbers[i];
+			}
+
+			sort(number_string.begin(), number_string.end());
+
+			do
+			{
+				int number = stoi(number_string);
+
+				if (find(number_set.begin(), number_set.end(), number) == number_set.end())
+				{
+					number_set.insert(number);
+
+					// 소수 체크
+
+					bool is_prime = true;
+
+					for (int i = 2; i <= ceil(number / 2); i++)
+					{
+						if (number % i == 0)
+						{
+							is_prime = false;
+							break;
+						}
+					}
+
+					if (number <= 1)
+						is_prime = false;
+
+					if (is_prime)
+					{
+						answer++;
+					}
+				}
+
+			} while (next_permutation(number_string.begin(), number_string.end()));
+		} while (next_permutation(comb.begin(), comb.end()));
+	}
+ 
+    return answer;
+}
