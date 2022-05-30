cfc2235482511275fd09fc8936999f403db078b5
yoonsangmin <dbstkdalsa@naver.com>
2022-05-30 18:30:13 +0900

    프로그래머스 자물쇠와 열쇠 다른 사람 풀이 참고

--- Coding-Test-Practice-C++/Programmers L3 - Lock and Key.cpp
+++ Coding-Test-Practice-C++/Programmers L3 - Lock and Key.cpp
@@ -40,3 +40,108 @@
 //
 //    return false;
 //}
+
+//// 다른 사람 풀이
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<vector<int>> rotate(vector<vector<int>> a) {
+//    int n = a.size();
+//    for (int i = 0; i < n / 2; i++) {
+//        for (int j = i; j < n - i - 1; j++) {
+//            int temp = a[i][j];
+//            a[i][j] = a[n - j - 1][i];
+//            a[n - j - 1][i] = a[n - 1 - i][n - 1 - j];
+//            a[n - 1 - i][n - 1 - j] = a[j][n - 1 - i];
+//            a[j][n - 1 - i] = temp;
+//        }
+//    }
+//    return a;
+//}
+//bool check(vector<vector<int>> v) {
+//    for (vector<int> vector : v) {
+//        for (int n : vector) {
+//            // 열쇠 구멍이 걸리는 경우 또는 구멍이 비어있는 경우
+//            if (n == 0 || n == -1) return false;
+//        }
+//    }
+//    return true;
+//}
+//
+//vector<vector<int>> matching(vector<vector<int>> key, vector<vector<int>> lock, int k, int l) {
+//    int lastIndexOfKey = key.size() - 1;
+//    int lastIndexOfLock = lock.size() - 1;
+//    int i = 0, j = 0, x = i - lastIndexOfKey + k, y = j - lastIndexOfKey + l;
+//    vector<vector<int>> temp = lock;
+//
+//    // 모든 열쇠 순회 - i, j는 키의 인덱스
+//    for (i = 0; i <= lastIndexOfKey; i++) {
+//        for (j = 0; j <= lastIndexOfKey; j++) {
+//            // x, y는 락의 인덱스
+//            x = i - lastIndexOfKey + k;
+//            y = j - lastIndexOfKey + l;
+//
+//            // 접근하려는 락이 범위 안에 있을 때만
+//            if (x >= 0 && x <= lastIndexOfLock && y >= 0 && y <= lastIndexOfLock) {
+//                // 비교
+//                // 열쇠가 락에 맞을 경우
+//                if (key[lastIndexOfKey - i][lastIndexOfKey - j] != lock[x][y]) {
+//                    temp[x][y] = 1;
+//                }
+//                // 열쇠가 락에 안 맞는 부분이 있는 경우
+//                else {
+//                    temp[0][0] = -1;
+//                    return temp;
+//                }
+//            }
+//        }
+//    }
+//    // 열쇠 구멍에 맞춘 후 맞춘 결과를 리턴해서 체크함 - 키랑 락이랑 안 겹친 부분에 비어있는 곳이 있을 수 도 있음
+//    return temp;
+//}
+//
+//bool insertKey(vector<vector<int>> key, vector<vector<int>> lock) {
+//    int lastIndexOfKey = key.size() - 1;
+//    int lastIndexOfLock = lock.size() - 1;
+//
+//    // 키와 락의 길이를 합친 것보다 한 번 적게 반복(키랑 락이랑 겹칠 수 있는 경우의 수)
+//    int times = lastIndexOfKey + lastIndexOfLock + 1;
+//
+//    for (int i = 0; i < times; i++) {
+//        for (int j = 0; j < times; j++) {
+//            if (check(matching(key, lock, i, j))) {
+//                return true;
+//            }
+//        }
+//    }
+//
+//    return false;
+//
+//
+//}
+//
+//bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
+//    bool answer = false;
+//
+//    // 90도씩 회전해서 4가지 키를 만듦
+//    vector<vector<int>> key1 = key;
+//    vector<vector<int>> key2 = rotate(key1);
+//    vector<vector<int>> key3 = rotate(key2);
+//    vector<vector<int>> key4 = rotate(key3);
+//
+//    if (insertKey(key1, lock)) {
+//        return true;
+//    }
+//    if (insertKey(key2, lock)) {
+//        return true;
+//    }
+//    if (insertKey(key3, lock)) {
+//        return true;
+//    }
+//    if (insertKey(key4, lock)) {
+//        return true;
+//    }
+//    return false;
+//}
