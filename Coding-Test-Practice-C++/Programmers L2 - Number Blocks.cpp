37a5046bf66e09bd3ac9c736082c6bf1e7d15741
yoonsangmin <dbstkdalsa@naver.com>
2022-06-05 16:42:17 +0900

    프로그래머스 숫자 블록 품

--- Coding-Test-Practice-C++/Programmers L2 - Number Blocks.cpp
+++ Coding-Test-Practice-C++/Programmers L2 - Number Blocks.cpp
@@ -1,25 +1,38 @@
-#include <string>
-#include <vector>
-#include <cmath>
-
-using namespace std;
-
-vector<int> solution(long long begin, long long end) {
-	long long n = end - begin + 1;
-	vector<int> answer(n, 0);
-
-	for (long long i = 0; i < n; i++)
-	{
-		long long num = begin + i;
-
-		for (long long j = 2; j < sqrt(num); j++)
-		{
-			if (num % j == 0)
-			{
-				answer[i] = num / j;
-			}
-		}
-	}
-
-    return answer;
-}
+//// ³» Ç®ÀÌ
+//#include <string>
+//#include <vector>
+//#include <cmath>
+//
+//using namespace std;
+//
+//vector<int> solution(long long begin, long long end) {
+//	long long size = end - begin + 1;
+//	vector<int> answer(size, 1);
+//
+//	if (begin == 1)
+//		answer[0] = 0;
+//
+//	for (long long i = 0; i < size; i++)
+//	{
+//		long long num = begin + i;
+//
+//		for (long long j = 2; j <= sqrt(num); j++)
+//		{
+//			long long quotient = num / j;
+//			if (num % j == 0 && quotient < 10000001)
+//			{
+//				answer[i] = quotient;
+//				break;
+//			}
+//		}
+//	}
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//	solution(999999999, 1000000000);
+//
+//	return 0;
+//}
